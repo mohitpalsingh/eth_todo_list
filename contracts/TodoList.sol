0b789a4bddf33be5063609aca4ad58f8fc9723af
Mohit Pal Singh <starktony@gmail.com>
2021-06-16 11:41:54 +0530

    basic tests done

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,23 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint public taskCount = 0;
+    uint public taskCount = 0; //state of blockchain
+
+    struct Task { //custom datastructure
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks; //hash
+
+    constructor() public {
+        createTask("Made by Mohit Pal Singh.");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false); //entry of task
+    }
+
 }
