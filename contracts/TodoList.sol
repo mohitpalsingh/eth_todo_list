3e00e2eac7d87bfcb7b260ea03d8bc04b9769302
Mohit Pal Singh <starktony@gmail.com>
2021-06-16 12:59:00 +0530

    complete tasks now --project completed

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -17,6 +17,11 @@ contract TodoList {
         bool completed
     );
 
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
     constructor() public {
         createTask("Made by Mohit Pal Singh.");
     }
@@ -27,4 +32,10 @@ contract TodoList {
         emit TaskCreated(taskCount, _content, false); //trigger activation
     }
 
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+    }
 }
