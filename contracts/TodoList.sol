e3232a44528adfc7cb9e9ddb008acb19b77de5da
Mohit Pal Singh <starktony@gmail.com>
2021-06-16 12:40:13 +0530

    creates tasks now

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,6 +11,12 @@ contract TodoList {
 
     mapping(uint => Task) public tasks; //hash
 
+    event TaskCreated(  //event-trigger
+        uint id,
+        string content,
+        bool completed
+    );
+
     constructor() public {
         createTask("Made by Mohit Pal Singh.");
     }
@@ -18,6 +24,7 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false); //entry of task
+        emit TaskCreated(taskCount, _content, false); //trigger activation
     }
 
 }
